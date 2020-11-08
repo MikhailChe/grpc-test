e3979ca8a43c71bd1ee561e57dcefc941c3f5072
Mikhail Chernoskutov <mikhail.chernoskutov@gmail.com>
2020-11-08 13:13:45 +0500

    Use generated proto

--- goclient/cmd/client/main.go
+++ goclient/cmd/client/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/mikhailche/grpc-test/fractalizer"
 )
 
 func main() {
 	fmt.Println("Hello, playground")
+	var point fractalizer.Point
+	point.X = 2
+	fmt.Println(point)
 }
